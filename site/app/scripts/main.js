b7c51fe8f9de97f8d35ed26c8cb2d91d57294e80
Ricardo Lafuente <r@manufacturaindependente.org>
2016-05-13 13:08:49 +0200

    Organise chart code and setup boxplot

--- site/app/scripts/main.js
+++ site/app/scripts/main.js
@@ -1,3 +1,5 @@
 $(document).ready(function() {    
-  var chart_one = new Chart('#chart-one', 'data/life_salary.csv');
+  var chart_one = new ChartOne('#chart-one', 'data/life_salary.csv');
+  var chart_two = new ChartTwo('#chart-two', 'data/wage_distribution.csv');
+  var chart_three = new ChartThree('#chart-three', 'data/life_salary.csv');
 });
