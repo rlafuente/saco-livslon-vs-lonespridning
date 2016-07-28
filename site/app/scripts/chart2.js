2bb23e35b7ecf995a13d2e06bfea9ea1c64f446f
Ricardo Lafuente <r@manufacturaindependente.org>
2016-07-28 16:31:25 +0200

    Separate charts into single files

---
+++ site/app/scripts/chart2.js
@@ -0,0 +1,192 @@
+/*
+ * Wage distribution
+ */
+
+ChartTwo = (function() {
+    function ChartTwo(selector, data, opts) {
+        var self = this;
+        self.container = d3.select(selector);
+        defaultOpts = {
+            isIframe: false
+        }
+        self.opts = $.extend(defaultOpts, opts);
+
+        // Inital state
+        self.data = data;
+
+        // Append chart container
+        self.chartContainer = self.container.append("div")
+            .attr("class", "chart-container");
+
+        // Render charts
+        self.drawChart();
+
+        // Do transitions
+        self.update(data);
+
+        // Make responsize
+        d3.select(window).on('resize', function() {
+            self.resize();
+        });
+    }
+    // Draw DOM elements
+    ChartTwo.prototype.drawChart = function() {
+        var self = this;
+        var containerWidth = self.container[0][0].offsetWidth;
+
+        // clear container
+        self.chartContainer.html("");
+
+        // Setup sizing
+        self.margins = m = {
+            top: containerWidth * 0.1,
+            right: containerWidth * 0.1,
+            bottom: containerWidth * 0.1,
+            left: containerWidth * 0.1 
+        };
+        self.width = w = containerWidth - m.left - m.right;
+        self.height = h = w * 0.5;
+        self.pointRadius = containerWidth * 0.01;
+        var fontSize = m.bottom * 0.7 + "px";
+
+        // margin value to make room for the y-axis
+        var xAxisMargin = 60;
+        var yAxisMargin = 60;
+
+        // Create SVG container
+        self.svg = self.chartContainer.append('svg')
+            .attr('width', '100%')
+            .attr('height', '100%')
+            .attr('viewBox','0 0 ' + self.width +' '+ self.height)
+            .attr("preserveAspectRatio", "xMinYMin meet");
+        self.chart = self.svg.append('g')
+            .attr('transform', 'translate(' + yAxisMargin + ',' + -xAxisMargin + ')');
+
+        var x = d3.scale.ordinal()
+              .rangeRoundBands([0, self.width - yAxisMargin], .1);
+        var y = d3.scale.linear()
+              .range([self.height-xAxisMargin, xAxisMargin*2]);
+
+        d3.csv(self.data, function (error, data) {
+          data = data.sort(function(a, b){ return d3.ascending(parseInt(a.median), parseInt(b.median)); });
+
+          x.domain(data.map(function(d) { return +d.median; }));
+          y.domain([0, d3.max(data, function(d) { return parseInt(d.P90); })]);
+          //y.domain([0, d3.max(data, function(d) { return parseInt(d.median); })]);
+          //y.domain([d3.min(data, function(d) { return parseInt(d.P10)}), d3.max(data, function(d) { return parseInt(d.P90); })]);
+          
+          // Initialize tooltip
+          var tip = d3.tip()
+            .attr('class', 'd3-tip')
+            .offset([-10, 0])
+            .html(function(d) {
+              return "<p><strong>Yrke</strong>: " + d.profession_name + "</p>" + 
+                     "<p><strong>Månadslön, lägst 10%</strong>: " + d.P10 + " kronor</p>" +
+                     "<p><strong>Månadslön, högsta 10%</strong>: " + d.P90 + " kronor</p>";
+            })
+          self.chart.call(tip);
+
+          var bar = self.chart.selectAll("g")
+              .data(data)
+            .enter().append("g")
+              .attr("width", 20)
+              .attr("transform", function(d) { return "translate(" + x(+d.median) + ",0)"; });
+
+          bar.append("rect")
+              .attr("class", function(d) { return "edges " + d.group; })
+              .attr("y", function(d) { return y(d.P90); })
+              .attr("height", function(d) { return self.height - y(parseInt(d.P90 - d.P10)); })
+              .attr("width", x.rangeBand())
+              .attr("rx", 3)
+              .attr("ry", 3)
+              .attr("fill", "#ECDAB5")
+
+          // quartiles
+          bar.append("rect")
+              .attr("class", function(d) { return "quartiles " + d.group; })
+              .attr("y", function(d) { return y(d._Q3); })
+              .attr("height", function(d) { return self.height - y(parseInt(d._Q3 - d._Q1)); })
+              .attr("width", x.rangeBand())
+              .attr("rx", 3)
+              .attr("ry", 3)
+              .attr("fill", "#BDA164");
+          
+          // median
+          bar.append("circle")
+              .attr("class", function(d) { return "median " + d.group; })
+              .attr("cy", function(d) { return y(d.median); })
+              .attr("r", function(d) { return 1; })
+              .attr("cx", x.rangeBand() / 2)
+              .attr("fill", "#F8F0DE")
+              .attr("id", function(d) { return d.profession_name });
+
+          // transparent overlay for tooltips
+          bar.append("rect")
+              .attr("class", function(d) { return "tip-overlay d3-tip " + d.group; })
+              .attr("y", function(d) { return y(d.P90); })
+              .attr("height", function(d) { return self.height - y(parseInt(d.P90 - d.P10)); })
+              .attr("width", x.rangeBand())
+              .style("opacity", "0")
+              .attr("rx", 3)
+              .attr("ry", 3)
+              .on('mouseover', function(d) {
+                $('#chart-two-title').text(d.profession_name);
+                $('#chart-two-subtitle-1').html("<strong>Månadslön, lägst 10%</strong>: " + d.P10 + " kronor");
+                $('#chart-two-subtitle-2').html("<strong>Månadslön, högsta 10%</strong>: " + d.P90 + " kronor");
+              })
+              .on('mouseout', function(d) {
+                $('#chart-two-title').text("Yrkesgrupp vs Månadslön");
+                $('#chart-two-subtitle-1').html("&nbsp;");
+                $('#chart-two-subtitle-2').html("&nbsp;");
+              });
+ 
+          var yAxis = d3.svg.axis() 
+            .scale(y)
+            .ticks(8, "s")
+            .orient("left");
+          self.svg.append("g")
+            .attr("class", "axis")
+            .attr("transform", "translate(" + yAxisMargin*1.2 + ", 0)")
+            .call(yAxis);
+          // remove tick for 0
+          d3.selectAll('g.tick')
+            .filter(function(d){ return d==0;} )
+            .select('text') //grab the tick line
+            .style('visibility', 'hidden');
+
+          self.svg.append("text")
+            .text("Yrkesgrupp")
+            .attr("class", "axis legend")
+            .style("background", "white")
+            .style("text-transform", "uppercase")
+            .attr("transform", "translate(" + yAxisMargin + "," + (self.height-xAxisMargin/3) + ")")
+            .style("text-anchor", "start");
+          self.svg.append("text")
+            .text("Månadslön")
+            .attr("class", "axis legend")
+            .attr("transform", "translate(" + yAxisMargin/2 + "," + (self.height-xAxisMargin) + ") rotate(-90)")
+            .style("text-anchor", "start")
+            .style("background-color", "white");
+         
+        });
+
+        // Send resize signal to parent page
+        if (self.opts.isIframe) {
+            pymChild.sendHeight();
+        }
+    }
+    // Transitions only
+    ChartTwo.prototype.update = function(data) {
+        var self = this;
+        self.data = data;
+
+    }
+    ChartTwo.prototype.resize = function() {
+        var self = this;
+        self.svg.remove();
+        self.drawChart();
+        self.update(self.data);
+    }
+    return ChartTwo;
+})();
+
---
+++ site/app/scripts/chart3.js
@@ -0,0 +1,158 @@
+/*
+ * Scatterplot
+ */
+
+ChartThree = (function() {
+    function ChartThree(selector, data, opts) {
+        var self = this;
+        self.container = d3.select(selector);
+        defaultOpts = {
+            isIframe: false
+        }
+        self.opts = $.extend(defaultOpts, opts);
+
+        // Inital state
+        self.data = data;
+
+        // Append chart container
+        self.chartContainer = self.container.append("div")
+            .attr("class", "chart-container");
+
+        // Render charts
+        self.drawChart();
+
+        // Do transitions
+        self.update(data);
+
+        // Make responsize
+        d3.select(window).on('resize', function() {
+            self.resize();
+        });
+    }
+    // Draw DOM elements
+    ChartThree.prototype.drawChart = function() {
+        var self = this;
+        var containerWidth = self.container[0][0].offsetWidth;
+
+        // clear container
+        self.chartContainer.html("");
+
+        // Setup sizing
+        self.margins = m = {
+            top: containerWidth * 0.1,
+            right: containerWidth * 0.1,
+            bottom: containerWidth * 0.1,
+            left: containerWidth * 0.1 
+        };
+        self.width = w = containerWidth - m.left - m.right;
+        self.height = h = w * 0.5;
+        self.pointRadius = containerWidth * 0.01;
+        var fontSize = m.bottom * 0.7 + "px";
+
+        // margin value to make room for the axes
+        var xAxisMargin = 60;
+        var yAxisMargin = 60;
+
+        // Create SVG container
+        self.svg = self.chartContainer.append('svg')
+            .attr('width', '100%')
+            .attr('height', '100%')
+            .attr('viewBox','0 0 ' + self.width +' '+ self.height)
+            .attr("preserveAspectRatio", "xMinYMin meet");
+        self.chart = self.svg.append('g')
+            .attr('transform', 'translate(' + yAxisMargin + ',' + -xAxisMargin + ')');
+
+        var x = d3.scale.linear()
+              .range([20, self.width - 20 - yAxisMargin]);
+        var y = d3.scale.linear()
+              .range([self.height-xAxisMargin, xAxisMargin*2]);
+
+        d3.csv(self.data, function (error, data) {
+          x.domain([d3.min(data, function(d) { return parseFloat(d.lifesalary_vs_baseline); }), 
+                    d3.max(data, function(d) { return parseFloat(d.lifesalary_vs_baseline); })]);
+          y.domain([d3.min(data, function(d) { return parseFloat(d.income_range); }), 
+                    d3.max(data, function(d) { return parseFloat(d.income_range); })]);
+ 
+          var dot = self.chart.selectAll("g")
+              .data(data)
+            .enter().append("g");
+
+          dot.append("circle")
+              .attr("class", function(d) { return "element d3-tip " + d.group; })
+              .attr("cx", function(d) { return x(parseFloat(d.lifesalary_vs_baseline)); })
+              .attr("cy", function(d) { return y(parseFloat(d.income_range)); })
+              .attr("r", self.width/100)
+              .attr("fill", "#F8F0DE")
+              .attr("stroke", "#BDA164")
+              .attr("stroke-width", "1")
+              .attr("fill-opacity", 0.2)
+
+              .on('mouseover', function(d) {
+                d3.select(this).attr('fill-opacity', 1);
+                $('#chart-three-title').text(d.profession_name);
+                $('#chart-three-subtitle-1').html(
+                    "<strong>Lönespridning (P90/P10)</strong>: " + Number(parseFloat(d.income_range).toFixed(2))
+                );
+                $('#chart-three-subtitle-2').html(
+                    "<strong>Livslön jämfört med gymnasieutbildad</strong>: " + Number(parseFloat(d.lifesalary_vs_baseline).toFixed(2)) + " procent"
+                );
+
+              })
+              .on('mouseout', function(d) {
+                d3.select(this).attr('fill-opacity', 0.2);
+                $('#chart-three-title').text("Livslön jämfört med gymnasieutbildad vs Lönespridning (P90/P10)");
+                $('#chart-three-subtitle-1').html("&nbsp;");
+                $('#chart-three-subtitle-2').html("&nbsp;");
+              });
+  
+          var xAxis = d3.svg.axis() 
+            .scale(x)
+            .ticks(Math.floor(self.width/120), "s")
+            .orient("bottom");
+          self.svg.append("g")
+            .attr("class", "axis")
+            .attr("transform", "translate(0," + (self.height-xAxisMargin) + ")")
+            .call(xAxis);
+          var yAxis = d3.svg.axis() 
+            .scale(y)
+            .ticks(Math.floor(self.width/120), "s")
+            .orient("left");
+          self.svg.append("g")
+            .attr("class", "axis")
+            .attr("transform", "translate(" + yAxisMargin*1.2 + ", 0)")
+            .call(yAxis);
+
+          self.svg.append("text")
+            .text("Lönespridning (P90/P10)")
+            .attr("class", "axis legend")
+            .style("background", "white")
+            .style("text-transform", "uppercase")
+            .attr("transform", "translate(" + yAxisMargin + "," + (self.height-xAxisMargin/3) + ")")
+            .style("text-anchor", "start");
+          self.svg.append("text")
+            .text("Livslön jämfört med gymnasieutbildad")
+            .attr("class", "axis legend")
+            .attr("transform", "translate(" + yAxisMargin/2 + "," + (self.height-xAxisMargin) + ") rotate(-90)")
+            .style("text-anchor", "start")
+            .style("background-color", "white");
+        });
+
+        // Send resize signal to parent page
+        if (self.opts.isIframe) {
+            pymChild.sendHeight();
+        }
+    }
+    // Transitions only
+    ChartThree.prototype.update = function(data) {
+        var self = this;
+        self.data = data;
+
+    }
+    ChartThree.prototype.resize = function() {
+        var self = this;
+        self.svg.remove();
+        self.drawChart();
+        self.update(self.data);
+    }
+    return ChartThree;
+})();
